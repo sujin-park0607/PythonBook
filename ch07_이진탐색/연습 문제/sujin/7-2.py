a75ba65f8a64e87ae87614f835012980e200bff7
sujin-park0607 <goweg0607@gmail.com>
2023-07-24 18:09:18 +0900

    떡볶이 떡 만들기

--- ch07_이진탐색/연습 문제/sujin/7-2.py
+++ ch07_이진탐색/연습 문제/sujin/7-2.py
@@ -14,19 +14,22 @@ def binary_search(array, m, start, end):
     if start > end:
         return None
     
+    # 절단기 높이 찾기
     height = (start + end) // 2
-    # print("height: ",height)
+    
+
+    # 절단한 떡의 길이 합 구하기
     height_sum = 0
     for i in array:
         if i > height:
             height_sum += i - height
-
-    # print("height_sum: ",height_sum)
-
+    # 요청한 떡의 길이와 절단한 떡의 길이가 같을 경우 반환
     if height_sum == m:
         return height
+    # 절단된 떡의 길이 < 요청한 떡의길이 -> 경우 절단기 높이를 줄임
     elif height_sum < m:
         return binary_search(array, m, start, height-1)
+    # 절단된 떡의 길이 > 요청한 떡의길이 -> 경우 절단기 높이를 늘림
     else:
         return binary_search(array, m, height+1, end)
 
@@ -35,6 +38,7 @@ def binary_search(array, m, start, end):
 N, M = map(int,input().split(" "))
 array = sorted(list(map(int, input().split(" "))))
 
-print(binary_search(array, M, 1, 1000000))
+# 절단기 길이 1~1000000범위를 모두 탐색
+print(binary_search(array, M, 0, max(array)))
         
 
