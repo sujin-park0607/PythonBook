9afd981a062261bff366d1a470fc2058e4491492
sujin-park0607 <goweg0607@gmail.com>
2023-08-01 18:30:11 +0900

    럭키 스트라이크

---
+++ ch04_ 구현/실전 문제/sujin/4-1.py
@@ -0,0 +1,34 @@
+# # 
+## 4-1 러키스트라이크
+
+## Solution
+
+### 문제의 아이디어 생각해낸 포인트
+# 문자열을 하나씩 배열에 넣기
+# 배열의 길이의 반을 나눈 후 왼쪽, 오른쪽 반복문으로 더하기
+
+### 시간 복잡도 계산
+# A. 평균: O(N)
+
+### 소요시간
+# 10M
+
+
+n = list(map(int, input()))
+length = len(n) // 2
+left_sum = 0
+right_sum = 0
+
+for l in n[:length]:
+    left_sum += l
+
+for r in n[length:]:
+    right_sum += r    
+
+if left_sum == right_sum: print("LUCKY")
+else: print("READY")
+
+
+
+
+
